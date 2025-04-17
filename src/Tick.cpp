17597a66b3acd08d6b61c678b43cb127bf9d62f0
Faceling11 <facelingnoefeito11@gmail.com>
2025-04-17 12:27:10 -0300

    Second

--- src/Tick.cpp
+++ src/Tick.cpp
@@ -1,10 +1,33 @@
 #ifdef TICK
 
-#include "../headers/Tick.h"
-void Tick::passTick()
+#include "Tick.h"
+size_t Tick::getCurrentTimeMS() 
 {
-  sleep(seconds{1});
-  ++currentTick;
+    return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 }
 
+void Tick::tickLoop() 
+{
+    size_t lastTickMS = getCurrentTimeMS();
+    while (1) 
+    {
+        size_t currentTimeMS = getCurrentTimeMS();
+        
+        bool passTick = currentTimeMS - lastTickMS >= tickIntervalMS;
+        if (passTick) 
+        {
+            // TICK LOGIC
+
+            lastTickMS = currentTimeMS;
+        }
+
+        size_t timeToNextTickMS = (lastTickMS + tickIntervalMS) - currentTimeMS;
+        sleep(milliseconds(timeToNextTickMS));
+    }
+}
+
+void Tick::startProgram()
+{
+    tickLoop();
+}
 #endif // TICK

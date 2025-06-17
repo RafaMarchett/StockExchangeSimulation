10edff95e1126e75a6dcff30bc0cf0f7700d58f5
RafaMarchett <rafaelsm22gamer@gmail.com>
2025-06-17 19:11:06 -0300

    Add MerketEvents

---
+++ headers/MarketEvents.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "header.hpp"
+#ifndef MARKETNEWS
+#define MARKETNEWS
+class marketEvents {
+public:
+  void generateRandomEvent();
+
+private:
+};
+
+#endif // MARKETNEWS
---
+++ src/MarketEvents.cpp
@@ -0,0 +1,27 @@
+#include "../headers/MarketEvents.h"
+
+void marketEvents::generateRandomEvent() {
+  int randomSeedEvent = (rand() % 100) + 1;
+  if (randomSeedEvent < 10) {
+    // NO MERCADO
+    // Aplicar evento, recebe uma string com setor
+    // Em todas as ações do setor, aplicar (positivo ou negativo)
+    // Market::aplicarEvento("Clothing");
+  }
+  if (randomSeedEvent < 20) {
+  }
+  if (randomSeedEvent < 30) {
+  }
+  if (randomSeedEvent < 40) {
+  }
+  if (randomSeedEvent < 50) {
+  }
+  if (randomSeedEvent < 60) {
+  }
+  if (randomSeedEvent < 70) {
+  } else {
+  }
+  // if randomEvent
+  // aplly in market
+  //
+}

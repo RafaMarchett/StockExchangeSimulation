163a5cec304766a19e7c1f9e7b180f85b09cc8d9
RafaMarchett <rafaelsm22gamer@gmail.com>
2025-06-18 13:38:34 -0300

    add allMembers

--- headers/MarketEvents.h
+++ headers/MarketEvents.h
@@ -2,11 +2,26 @@
 #include "header.hpp"
 #ifndef MARKETNEWS
 #define MARKETNEWS
+
+namespace _marketEvents {
+struct allMembers {
+  string eventSector;
+  unsigned eventTime;
+
+  auto toTuple() { return std::tie(eventSector, eventTime); }
+};
+} // namespace _marketEvents
+
 class marketEvents {
 public:
   void generateRandomEvent();
 
+  _marketEvents::allMembers getAllMembers();
+  void setAllMembers(const _marketEvents::allMembers &);
+
 private:
+  string eventSector = "";
+  unsigned eventTime;
 };
 
 #endif // MARKETNEWS
--- src/MarketEvents.cpp
+++ src/MarketEvents.cpp
@@ -1,8 +1,22 @@
 #include "../headers/MarketEvents.h"
 
+_marketEvents::allMembers marketEvents::getAllMembers() {
+  _marketEvents::allMembers allMembers;
+  allMembers.eventSector = eventSector;
+  allMembers.eventTime = eventTime;
+
+  return allMembers;
+}
+void marketEvents::setAllMembers(const _marketEvents::allMembers &inputStruct) {
+  eventSector = inputStruct.eventSector;
+  eventTime = inputStruct.eventTime;
+}
 void marketEvents::generateRandomEvent() {
   int randomSeedEvent = (rand() % 100) + 1;
+  if (randomSeedEvent < 50)
+  /*
   if (randomSeedEvent < 10) {
+
     // TODO: Aplicar evento...
     // NO MERCADO
     // Aplicar evento, recebe uma string com setor
@@ -10,6 +24,7 @@ void marketEvents::generateRandomEvent() {
     // Market::aplicarEvento("Clothing");
   }
   if (randomSeedEvent < 20) {
+    // aplly in market("Sector")
   }
   if (randomSeedEvent < 30) {
   }
@@ -22,7 +37,5 @@ void marketEvents::generateRandomEvent() {
   if (randomSeedEvent < 70) {
   } else {
   }
-  // if randomEvent
-  // aplly in market
-  //
+  */
 }

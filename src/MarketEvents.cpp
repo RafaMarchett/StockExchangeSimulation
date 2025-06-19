b5b3419908c47e80e462c5f123d157e07af5e143
RafaMarchett <rafaelsm22gamer@gmail.com>
2025-06-19 11:23:48 -0300

    gets and change generateRandomEvent logic

--- src/MarketEvents.cpp
+++ src/MarketEvents.cpp
@@ -1,6 +1,12 @@
 #include "../headers/MarketEvents.h"
 #include "../headers/Market.h"
 
+string marketEvents::getCurrentEventSector() const {
+  return currentEventSector;
+}
+Ternary marketEvents::getBull_or_recessionMarket() const {
+  return bull_or_recessionMarket;
+}
 marketEvents &marketEvents::getMarketEvents() {
   static marketEvents marketEventsInstance;
   return marketEventsInstance;
@@ -20,21 +26,26 @@ void marketEvents::setAllMembers(const _marketEvents::allMembers &inputStruct) {
   bull_or_recessionMarket = inputStruct.bull_or_recessionMarket_value;
 }
 void marketEvents::generateRandomEvent() {
-  int randomSeedEvent = (rand() % 100) + 1;
-  if (randomSeedEvent < 50) {
+  passTick();
+  if (eventTime <= 0) {
     currentEventSector = "";
-    return;
   }
-  Market &marketInstance = Market::getMarket();
-  vector<string> allSectors = marketInstance.getAllSectors();
-  int randomSector = rand() % allSectors.size();
-  bull_or_recessionMarket = rand() % 2 - 1;
-  eventTime = 300;
-  if (bull_or_recessionMarket.get() == 0) {
-    bull_or_recessionMarket = 1;
+  if (currentEventSector == "") {
+    int randomSeedEvent = (rand() % 100) + 1;
+    if (randomSeedEvent < 50) {
+      currentEventSector = "";
+      return;
+    }
+    Market &marketInstance = Market::getMarket();
+    vector<string> allSectors = marketInstance.getAllSectors();
+    int randomSector = rand() % allSectors.size();
+    bull_or_recessionMarket = rand() % 2 - 1;
+    eventTime = 300;
+    if (bull_or_recessionMarket.get() == 0) {
+      bull_or_recessionMarket = 1;
+    }
+    currentEventSector = allSectors[randomSector];
   }
-  currentEventSector = allSectors[randomSector];
-  return;
 }
 void marketEvents::passTick() {
   if (eventTime > 0)

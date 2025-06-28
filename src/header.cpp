bcdca685283d13922903f0a96ab9a456a64fed75
RafaMarchett <rafaelsm22gamer@gmail.com>
2025-06-28 17:11:55 -0300

    Add headers.cpp

---
+++ src/header.cpp
@@ -0,0 +1,30 @@
+#include "../headers/header.hpp"
+#include "../headers/SystemFunctions.h"
+void initializeLanguage() {
+  char opt{'/'};
+  SysFuncs SysFuncsManager;
+  cout << clear;
+  do {
+    cout << "\nEnter 1 for English\n"
+         << "Insira 2 para Portugues\n>>> " << std::flush;
+    opt = SysFuncsManager.getSingleKey();
+    if (opt == '1' || opt == '2') {
+      language = opt;
+      break;
+    }
+    cout << clear << "\nEnter a valid option\nInsira uma opção válida\n"
+         << std::flush;
+  } while (1);
+}
+
+void printInLanguage(const string &string1, const string &string2) {
+  if (language == '1') {
+    cout << string1;
+  } else if (language == '2') {
+    cout << string2;
+  } else {
+    initializeLanguage();
+    // exit(LANGUAGE_NOT_FIND);
+  }
+  cout << std::flush;
+}
